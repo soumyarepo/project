6ae84401ef3dc6c0d1459a771bd40c3e8240e682
soumyarepo <ranjan.soumya8055@gmail.com>
2023-08-03 23:06:58 +0530

    projectfiles

--- multipleinstance/instance.tf
+++ multipleinstance/instance.tf
@@ -36,6 +36,14 @@ resource "aws_security_group" "demo-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "JENKINSPORT"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "SSH"
     from_port   = 0
